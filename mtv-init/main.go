cc7dbf46c3e3390caad5375a3cb64821c5feb767
Roy Golan <rgolan@redhat.com>
2024-09-30 12:34:57 +0300

    Improvments to mtv-init form
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- mtv-init/main.go
+++ mtv-init/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+    _ "embed"
 	"fmt"
 	"html/template"
 	"io"
@@ -27,7 +28,7 @@ func main() {
 }
 
 func bootstrapHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.New("form").Parse(fff)
+	tmpl, err := template.New("form").Parse(indexhtml)
 	if err != nil {
 		http.Error(w, "Unable to load form", http.StatusInternalServerError)
 		return
@@ -93,63 +94,8 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
         return 
     }
 	// For now, just return a simple confirmatio
-	fmt.Fprintf(w, "<html><body>File and vmware credentials recieved and avaiable under <a href=\"/vmware\" />/vmware</a></body></html>\n")
+	fmt.Fprintf(w, "<html><body>vddk.tar.gz and vmware credentials recieved and avaiable under <a href=\"/vmware\" />/vmware</a></body></html>\n")
 }
 
-const fff = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="fonts.css" />
-  <!-- Include latest PatternFly CSS via CDN -->
-  <link rel="stylesheet" href="https://unpkg.com/@patternfly/patternfly/patternfly.css" crossorigin="anonymous" >
-  <link rel="stylesheet" href="style.css" />
-  <title>PatternFly Basic CodeSandbox Example</title>
-</head>
-<body>
-  <header class="pf-v5-c-masthead" id="basic-masthead">
-  <span class="pf-v5-c-masthead__toggle">
-    <button
-      class="pf-v5-c-button pf-m-plain"
-      type="button"
-      aria-label="Global navigation"
-    >
-      <i class="fas fa-bars" aria-hidden="true"></i>
-    </button>
-  </span>
-  <div class="pf-v5-c-masthead__main">
-    <a class="pf-v5-c-masthead__brand" href="#">Migration Preparations</a>
-  </div>
-  <div class="pf-v5-c-masthead__content">
-  </div>
-  </header>
-   <!-- Main content -->
-   <main class="pf-c-page__main">
-       <div class="pf-c-form">
-           <h1 class="pf-c-title pf-m-2xl"></h1>
-           <form action="/upload" method="post" enctype="multipart/form-data" class="pf-c-form">
-               <div class="pf-c-form__group">
-                   <label for="vddk" class="pf-c-form__label">VDDK File</label>
-                   <input type="file" name="vddk" id="vddk" class="pf-c-form-control" required />
-               </div>
-               <div class="pf-c-form__group">
-                   <label for="username" class="pf-c-form__label">vSphere Username</label>
-                   <input type="text" name="username" id="username" class="pf-c-form-control" required />
-               </div>
-               <div class="pf-c-form__group">
-                   <label for="password" class="pf-c-form__label">vSphere Password</label>
-                   <input type="password" name="password" id="password" class="pf-c-form-control" required />
-               </div>
-               <div class="pf-c-form__group">
-                   <label for="url" class="pf-c-form__label">vSphere URL</label>
-                   <input type="url" name="url" id="url" class="pf-c-form-control" required />
-               </div>
-               <button type="submit" class="pf-c-button pf-m-primary">Submit</button>
-           </form>
-       </div>
-   </main>
-</body>
-</html>
-`
+//go:embed index.html
+var indexhtml string 
